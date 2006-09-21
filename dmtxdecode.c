7590ea36900b877bcabb1663dca767c86ccd94cc
Mike Laughton <mike@dragonflylogic.com>
2006-09-21 05:19:05 +0000

    Removed callback functions and fine tuned Base256 decode.

--- dmtxdecode.c
+++ dmtxdecode.c
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 Contact: mike@dragonflylogic.com
 */
 
-/* $Id: dmtxdecode.c,v 1.6 2006-09-20 05:35:05 mblaughton Exp $ */
+/* $Id: dmtxdecode.c,v 1.7 2006-09-21 05:19:05 mblaughton Exp $ */
 
 /**
  *
@@ -98,171 +98,6 @@ dmtxScanStartNew(DmtxDecode *decode)
       dmtxMatrixRegionDeInit(&(decode->matrix[i]));
 
    decode->matrixCount = 0;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetStepScanCallback(DmtxDecode *decode, void (* func)(DmtxDecode *, DmtxScanRange *, DmtxJumpScan *))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->stepScanCallback = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetCrossScanCallback(DmtxDecode *decode, void (* func)(DmtxScanRange *, DmtxGradient *, DmtxEdgeScan *))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->crossScanCallback = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetFollowScanCallback(DmtxDecode *decode, void (* func)(DmtxEdgeFollower *))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->followScanCallback = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetFinderBarCallback(DmtxDecode *decode, void (* func)(DmtxRay2 *))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->finderBarCallback = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetBuildMatrixCallback2(DmtxDecode *decode, void (* func)(DmtxFinderBar *, DmtxMatrixRegion *))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->buildMatrixCallback2 = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetBuildMatrixCallback3(DmtxDecode *decode, void (* func)(DmtxMatrix3))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->buildMatrixCallback3 = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetBuildMatrixCallback4(DmtxDecode *decode, void (* func)(DmtxMatrix3))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->buildMatrixCallback4 = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetPlotPointCallback(DmtxDecode *decode, void (* func)(DmtxVector2, int, int, int))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->plotPointCallback = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetXfrmPlotPointCallback(DmtxDecode *decode, void (* func)(DmtxVector2, DmtxMatrix3, int, int))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->xfrmPlotPointCallback = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetFinalCallback(DmtxDecode *decode, void (* func)(DmtxMatrixRegion *))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->finalCallback = func;
-}
-
-/**
- *
- * @param XXX
- * @return XXX
- */
-extern void
-dmtxSetPlotModuleCallback(DmtxDecode *decode, void (* func)(DmtxDecode *, DmtxMatrixRegion *, int, int, DmtxVector3))
-{
-   if(decode == NULL) {
-      exit(1); // XXX better error handling strategy
-   }
-
-   decode->plotModuleCallback = func;
 }
 
 /**
@@ -808,14 +643,14 @@ DecodeSchemeBase256(DmtxMatrixRegion *matrixRegion, unsigned char *ptr, unsigned
 
    d0 = UnRandomize255State(code, i++);
    if(d0 == 0) {
-      iEnd = dataEnd - code;
+      iEnd = dataEnd - code; // XXX not verifed
    }
    else if(d0 <= 249) {
-      iEnd = iBeg + d0;
+      iEnd = iBeg + d0 + 1; // XXX verifed
    }
    else {
       d1 = UnRandomize255State(code, i++);
-      iEnd = iBeg + (d0 - 249) * 250 + d1;
+      iEnd = iBeg + (d0 - 249) * 250 + d1; // XXX not verified
    }
 
    if(iEnd > dataEnd - code) {
