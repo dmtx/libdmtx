d7d14218a36d5bf78b786826c0d334dccfa9be47
Mike Laughton <mike@dragonflylogic.com>
2006-10-13 01:58:49 +0000

    Implemented initial TODO counts.

--- script/check_todo.sh
+++ script/check_todo.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-echo "not implemented yet" > /dev/null
-ERR=$?
+FILES=$(find . -type f -name "*.[ch]" -o -name "*.sh")
 
-exit $ERR
+for file in $FILES; do
+
+   TODO_COUNT=$(grep -in -e "XXX" -e "TODO" -e "FIXME" $file | wc -l)
+   if [[ "$TODO_COUNT" -gt 0 ]]; then
+      printf "%4d TODO(s) remain in $file\n" $TODO_COUNT
+   fi
+
+done
+
+exit 0
