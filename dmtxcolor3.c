ad85a9d6a706d8624de9314f2b27750f7cb71e73
Mike Laughton <mike@dragonflylogic.com>
2008-01-18 22:02:05 +0000

    Updated copyright label

--- dmtxcolor3.c
+++ dmtxcolor3.c
@@ -1,7 +1,7 @@
 /*
 libdmtx - Data Matrix Encoding/Decoding Library
 
-Copyright (c) 2007 Mike Laughton
+Copyright (c) 2008 Mike Laughton
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 Contact: mike@dragonflylogic.com
 */
 
-/* $Id: dmtxcolor3.c,v 1.3 2006/10/12 18:04:27 mblaughton Exp $ */
+/* $Id$ */
 
 /**
  *
