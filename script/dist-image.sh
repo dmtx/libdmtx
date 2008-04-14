17dcaeef1ce95b5cb9fbf2e741540e5120800426
Mike Laughton <mike@dragonflylogic.com>
2008-04-14 02:28:51 +0000

    Final updates before release

--- script/dist-image.sh
+++ script/dist-image.sh
@@ -5,10 +5,7 @@ VERSION=$(grep AC_INIT configure.ac | awk '{print $2}' | sed 's/[^0-9\.]//g')
 SRC_DIST=libdmtx-$VERSION
 IMG_DIST=libdmtx-images-$VERSION
 
-IMG_DIRS=" \
-$SRC_DIST/test/images_core \
-$SRC_DIST/test/images_omar \
-$SRC_DIST/test/images_sportcam"
+IMG_DIRS="$SRC_DIST/test/images_public"
 
 tar --exclude CVS -cvf $IMG_DIST.tar -C .. $IMG_DIRS
 
