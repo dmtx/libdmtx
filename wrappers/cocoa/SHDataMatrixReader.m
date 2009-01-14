1a4fb5e229370c3fec41ccb5a209a1b94707868f
Mike Laughton <mike@dragonflylogic.com>
2009-01-14 17:11:17 +0000

    Early updates

--- wrappers/cocoa/SHDataMatrixReader.m
+++ wrappers/cocoa/SHDataMatrixReader.m
@@ -47,7 +47,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 - (id)init {
 	self = [super init];
 	if(self != nil) {
-		
+
 	}
 	return self;
 }
@@ -62,60 +62,56 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #else
 - (NSString *)decodeBarcodeFromImage:(NSImage *)image {
 #endif
-	
+
 	NSMutableArray *messages = [NSMutableArray array];
-	
+
 	// Create dmtx image.
 	DmtxImage *dmtxImage = [self scaledDmtxImageForImage:image];
 	if(dmtxImage == NULL)
 		return nil;
-	
-	// Define dmtx active search area.
-	DmtxPixelLoc dmtxPixelLoc0, dmtxPixelLoc1;
-	dmtxPixelLoc0.X = dmtxPixelLoc0.Y = 0;
-	dmtxPixelLoc1.X = dmtxImage->width - 1;
-	dmtxPixelLoc1.Y = dmtxImage->height - 1;
-	
+
 	// Initialize dmtx decode struct for image.
-	DmtxDecode dmtxDecode = dmtxDecodeStructInit(dmtxImage, dmtxPixelLoc0, dmtxPixelLoc1, 2);
-	
-	DmtxRegion dmtxRegion;
+	DmtxDecode *dmtxDecode = dmtxDecodeCreate(dmtxImage);
+
+	DmtxRegion *dmtxRegion;
 	DmtxMessage *dmtxMessage;
-	
+
 	// Loop once for each detected barcode region.
 	NSUInteger count;
 	for(count = 0; count < 1; count++) {
-		
+
 		// Find next barcode region within image.
-		dmtxRegion = dmtxDecodeFindNextRegion(&dmtxDecode);
-		if(dmtxRegion.found == DMTX_REGION_EOF)
+		dmtxRegion = dmtxRegionFindNext(dmtxDecode, NULL);
+		if(dmtxRegion == NULL)
 			break;
-		
+
 		// Decode region based on requested scan mode.
-		dmtxMessage = dmtxDecodeMatrixRegion(&dmtxDecode, &dmtxRegion, 1);
-		if(dmtxMessage == NULL)
-			continue;
-		
-		// Convert C string to NSString.
-		NSString *message = [NSString stringWithCString:(const char*)dmtxMessage->output length:(NSUInteger)dmtxMessage->outputIdx];
-		[messages addObject:message];
-				
-		// Free dmtx message memory.
-		dmtxMessageFree(&dmtxMessage);
+		dmtxMessage = dmtxDecodeMatrixRegion(dmtxDecode, dmtxRegion, -1);
+
+		if(dmtxMessage != NULL) {
+			// Convert C string to NSString.
+			NSString *message = [NSString stringWithCString:(const char*)dmtxMessage->output length:(NSUInteger)dmtxMessage->outputIdx];
+			[messages addObject:message];
+
+			// Free dmtx message memory.
+			dmtxMessageDestroy(&dmtxMessage);
+		}
+
+		dmtxRegionDestroy(&dmtxRegion);
+
 		break;
 	}
-	
+
 	// Free dmtx decode memory.
-	dmtxDecodeStructDeInit(&dmtxDecode);
-	
+	dmtxDecodeDestroy(&dmtxDecode);
+
 	// Free dmtx image memory.
-	dmtxImageFree(&dmtxImage);
-	
+	dmtxImageDestroy(&dmtxImage);
+
 	if([messages count] > 0)
 		return [messages objectAtIndex:0];
 	else
 		return nil;
-	
 }
 
 #if TARGET_OS_IPHONE
@@ -123,32 +119,32 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #else
 - (DmtxImage *)scaledDmtxImageForImage:(NSImage *)image {
 #endif
-	
+
 #if TARGET_OS_IPHONE
 	// We have to deal with a CGImage.
 	CGImageRef imageRef = image.CGImage;
 #else
 	CGImageRef imageRef = [[[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation]] CGImage];
 #endif
-	
+
 	// Calculate image dimensions (500 pixel should be enough for decoding).
 	NSUInteger aspectRatio = CGImageGetWidth(imageRef) / CGImageGetHeight(imageRef);
 	NSUInteger width = 500;
 	NSUInteger height = 500 / aspectRatio;
 	NSUInteger bytesPerRow = width * 4;
-	
+
 	// Create color space object.
 	CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
 	if(colorSpaceRef == NULL)
 		return NULL;
-	
+
 	// Create context memory.
 	void *memory = malloc(bytesPerRow * height);
 	if(memory == NULL) {
 		CGColorSpaceRelease(colorSpaceRef);
 		return NULL;
 	}
-	
+
 	// Create bitmap context.
 	CGContextRef contextRef = CGBitmapContextCreate(memory, width, height, 8, bytesPerRow, colorSpaceRef, kCGImageAlphaPremultipliedFirst);
 	if(contextRef == NULL) {
@@ -156,13 +152,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 		free(memory);
 		return NULL;
 	}
-	
+
 	// Release color space object.
 	CGColorSpaceRelease(colorSpaceRef);
-	
+
 	// Scale image to desired size.
 	CGContextDrawImage(contextRef, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), imageRef);
-	
+
 	// Get context data.
 	unsigned char *data = (unsigned char *)CGBitmapContextGetData(contextRef);
 	if(data == NULL) {
@@ -170,7 +166,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 		free(memory);
 		return NULL;
 	}
-	
+
 	// Create dmtx image.
 	DmtxImage *dmtxImage = dmtxImageMalloc((int)width, (int)height);
 	if(dmtxImage == NULL) {
@@ -178,7 +174,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 		free(memory);
 		return NULL;
 	}
-	
+
 	// Copy horizontally flipped image data.
 	NSUInteger row, column, index;
 	for(row = 0; row < height; row++) {
@@ -189,15 +185,15 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 			dmtxImage->pxl[index].B = data[row * width * 4 + column * 4 + 3];
 		}
 	}
-	
+
 	// Release bitmap context.
 	CGContextRelease(contextRef);
-	
+
 	// Free context memory.
 	free(memory);
-	
+
 	return dmtxImage;
-	
+
 }
 
 @end
