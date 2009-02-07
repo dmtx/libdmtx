81729ea2f8d51ea104f36906f4ef1e36276f3c72
Joseph Ferner <joegtp@users.sourceforge.net>
2009-02-07 15:54:24 +0000

    updated copyright and read me

--- wrapper/net/libdmtx.c
+++ wrapper/net/libdmtx.c
@@ -1,7 +1,7 @@
 /*
 libdmtx-net - .NET wrapper for libdmtx
 
-Copyright (C) 2009 Tom Vali
+Copyright (C) 2009 Joseph Ferner /Tom Vali
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-Contact: mike@dragonflylogic.com
+Contact: libdmtx@fernsroth.com
 */
 
 /* $Id$ */
