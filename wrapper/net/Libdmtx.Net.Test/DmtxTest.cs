0a5dcb418ffbd66cac9d54001725990a94e45d5c
Joseph Ferner <joegtp@users.sourceforge.net>
2009-02-12 17:21:54 +0000

    added copyright to test classes.

--- wrapper/net/Libdmtx.Net.Test/DmtxTest.cs
+++ wrapper/net/Libdmtx.Net.Test/DmtxTest.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿/*
+libdmtx-net - .NET wrapper for libdmtx
+
+Copyright (C) 2009 Joseph Ferner / Tom Vali
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Contact: libdmtx@fernsroth.com
+*/
+
+/* $Id:$ */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
