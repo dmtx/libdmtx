4a52ce81ed41e83ca19734ebac5cf91b1f830fcc
Mike Laughton <mike@dragonflylogic.com>
2010-04-29 21:20:48 +0000

    Added grid alignment dots

--- test/multi_test/multi_test.c
+++ test/multi_test/multi_test.c
@@ -2056,16 +2056,42 @@ DrawNormalizedRegion(SDL_Surface *screen, SDL_Surface *picture,
                ptrFit[0] = ptrRaw[0]/2;
                ptrFit[1] = ptrRaw[1]/2;
                ptrFit[2] = ptrRaw[2]/2;
+               if((int)(x + 8*pCtr.X) % 8 == 0 && (int)(yFlip + 8*pCtr.Y) % 8 == 0) {
+                  ptrFit[0] = (ptrFit[0] < 150 ? 255 : 0);
+                  ptrFit[1] = (ptrFit[1] < 150 ? 255 : 0);
+                  ptrFit[2] = (ptrFit[2] < 150 ? 255 : 0);
+               }
             }
             else {
                ptrFit[0] = ptrRaw[0];
                ptrFit[1] = ptrRaw[1];
                ptrFit[2] = ptrRaw[2];
+               if((int)(x + 8*pCtr.X) % 8 == 0 && (int)(yFlip + 8*pCtr.Y) % 8 == 0) {
+                  ptrFit[0] = (ptrFit[0] < 150 ? 255 : 0);
+                  ptrFit[1] = (ptrFit[1] < 150 ? 255 : 0);
+                  ptrFit[2] = (ptrFit[2] < 150 ? 255 : 0);
+               }
             }
          }
       }
    }
    SDL_UnlockSurface(picture);
+/*
+   for(y = pCtr.Y * -8; y < extent; y += 8) {
+      yFlip = extent - 1 - y;
+      if(yFlip < 0) continue;
+
+      for(x = pCtr.X * -8; x < extent; x += 8) {
+         if(x < 0) continue;
+
+         ptrFit = pixbuf + (yFlip * bytesPerRow + x * 3);
+
+         ptrFit[0] += (255 - ptrFit[0])/2;
+         ptrFit[1] += (255 - ptrFit[1])/2;
+         ptrFit[2] += (255 - ptrFit[2])/2;
+      }
+   }
+*/
 
    clipRect.w = extent;
    clipRect.h = extent;
