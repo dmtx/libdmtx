e87b6857a1d968590b9d96ab7d122e9f4972de08
Mike Laughton <mike@dragonflylogic.com>
2011-03-03 10:11:45 -0600

    Added autogen.sh test to create m4 directory

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+# Create empty m4 directory if missing
+if [[ ! -d "m4" ]]; then
+   echo "autogen.sh: creating empty m4 directory"
+   mkdir m4
+fi
+
+echo "autogen.sh: running autoreconf"
 autoreconf --force --install
