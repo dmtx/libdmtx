a1ac599db0f34dd65a59f392243fad318b81f123
Mike Laughton <mike@dragonflylogic.com>
2011-06-01 22:50:10 -0500

    Updated version number to 0.7.4

--- dmtx.h
+++ dmtx.h
@@ -32,7 +32,7 @@ extern "C" {
 #define M_PI_2    1.57079632679489661923
 #endif
 
-#define DmtxVersion              "0.7.3"
+#define DmtxVersion              "0.7.4"
 
 #define DmtxUndefined                 -1
 
